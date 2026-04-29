991bd48c45ce93df4f015ee80237f0f8a63d25c9
Coast <j.carmona@my.metroed.net>
2026-04-29 15:20:58 -0700

    Create script.py

---
+++ Most cookies/script.py
@@ -0,0 +1,33 @@
+bash
+cat << 'EOF' > wordlist.txt
+snickerdoodle
+chocolate chip
+oatmeal raisin
+gingersnap
+shortbread
+peanut butter
+whoopie pie
+sugar
+molasses
+kiss
+biscotti
+butter
+spritz
+snowball
+drop
+thumbprint
+pinwheel
+wafer
+macaroon
+fortune
+crinkle
+icebox
+gingerbread
+tassie
+lebkuchen
+macaron
+black and white
+white chocolate macadamia
+EOF
+Use code with caution.
+Why this is important: The cat << 'EOF' > wordlist.txt part tells your computer: "Take everything I’m about to type and save it into a file called wordlist.txt until I hit the end (EOF)." This creates the list of 28 possible passwords for the cracking tool to test.
