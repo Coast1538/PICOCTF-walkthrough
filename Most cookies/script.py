cf58fada147b751643d7b4f971ea8a04e4886c63
Coast <j.carmona@my.metroed.net>
2026-04-29 15:23:35 -0700

    Update script.py

--- Most cookies/script.py
+++ Most cookies/script.py
@@ -1,4 +1,3 @@
-bash
 cat << 'EOF' > wordlist.txt
 snickerdoodle
 chocolate chip
@@ -29,5 +28,4 @@ macaron
 black and white
 white chocolate macadamia
 EOF
-Use code with caution.
-Why this is important: The cat << 'EOF' > wordlist.txt part tells your computer: "Take everything I’m about to type and save it into a file called wordlist.txt until I hit the end (EOF)." This creates the list of 28 possible passwords for the cracking tool to test.
+# Why this is important: The cat << 'EOF' > wordlist.txt part tells your computer: "Take everything I’m about to type and save it into a file called wordlist.txt until I hit the end (EOF)." This creates the list of 28 possible passwords for the cracking tool to test.
